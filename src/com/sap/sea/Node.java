625b874d21c447a23c98ed4cb6512aef63535d23
I302472 <I302472@PVGN50863228A.apj.global.corp.sap>
2015-02-11 17:04:49 +0800

    add function check url

--- src/com/sap/sea/Node.java
+++ src/com/sap/sea/Node.java
@@ -4,9 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import javax.security.sasl.AuthenticationException;
 import javax.ws.rs.GET;
@@ -18,8 +15,6 @@ import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Response;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Strings;
 import com.trilead.ssh2.Connection;
@@ -31,9 +26,8 @@ public class Node {
 	public static final String FREE_GREP_MEM_AWK_PRINT_$2 = "free | grep Mem: | awk  '{print $2}'";
 	public static final String FREE_GREP_MEM_AWK_PRINT_$2_$3 = "free | grep Mem: | awk  '{print $3/$2 }'";
 	public static final String FREE_GREP_MEM_AWK_PRINT_$4 = "free | grep Mem: | awk  '{print $4 }'";
-	public final Logger logger = LoggerFactory.getLogger(Node.class);
-	private ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
-	private Connection connection;
+
+	public Connection connection;
 
 	private Island island;
 
@@ -49,8 +43,7 @@ public class Node {
 			return Response.ok(str).build();
 		} catch (IOException e) {
 			e.printStackTrace();
-			return Response.serverError()
-					.entity(ExceptionUtils.getStackTrace(e)).build();
+			return Response.serverError().entity(ExceptionUtils.getStackTrace(e)).build();
 		}
 	}
 
@@ -61,8 +54,7 @@ public class Node {
 			return Response.ok(runSh(FREE_GREP_MEM_AWK_PRINT_$2_$3)).build();
 		} catch (IOException e) {
 			e.printStackTrace();
-			return Response.serverError()
-					.entity(ExceptionUtils.getStackTrace(e)).build();
+			return Response.serverError().entity(ExceptionUtils.getStackTrace(e)).build();
 		}
 	}
 
@@ -73,8 +65,7 @@ public class Node {
 			return Response.ok(runSh(FREE_GREP_MEM_AWK_PRINT_$2)).build();
 		} catch (IOException e) {
 			e.printStackTrace();
-			return Response.serverError()
-					.entity(ExceptionUtils.getStackTrace(e)).build();
+			return Response.serverError().entity(ExceptionUtils.getStackTrace(e)).build();
 		}
 	}
 
@@ -85,8 +76,7 @@ public class Node {
 			return Response.ok(runSh(FREE_GREP_MEM_AWK_PRINT_$3)).build();
 		} catch (IOException e) {
 			e.printStackTrace();
-			return Response.serverError()
-					.entity(ExceptionUtils.getStackTrace(e)).build();
+			return Response.serverError().entity(ExceptionUtils.getStackTrace(e)).build();
 		}
 	}
 
@@ -97,8 +87,7 @@ public class Node {
 			return Response.ok(runSh(FREE_GREP_MEM_AWK_PRINT_$4)).build();
 		} catch (IOException e) {
 			e.printStackTrace();
-			return Response.serverError()
-					.entity(ExceptionUtils.getStackTrace(e)).build();
+			return Response.serverError().entity(ExceptionUtils.getStackTrace(e)).build();
 		}
 	}
 
@@ -109,22 +98,34 @@ public class Node {
 			return Response.ok(runSh("hostname")).build();
 		} catch (IOException e) {
 			e.printStackTrace();
-			return Response.serverError()
-					.entity(ExceptionUtils.getStackTrace(e)).build();
+			return Response.serverError().entity(ExceptionUtils.getStackTrace(e)).build();
 		}
 	}
+	
+	@GET
+	@Path("checkurl")
+	public Response checkurl(@QueryParam("url") final String url){
+		try {
+			String command = "curl -k -X HEAD " + url;
+			//String command = "curl -k -X HEAD http://10.58.77.129:8089/?view&s=3&key=10.58.136.164%3A7777";
+			System.out.println("command ::"+command);
+			return Response.ok(runSh(command)).build();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return Response.serverError().entity(ExceptionUtils.getStackTrace(e)).build();
+		}
+	}
+	
 
 	@POST
 	@Path("build")
-	public void build(final String buildPath,
-			@QueryParam("name") final String name,
+	public void build(final String buildPath, @QueryParam("name") final String name,
 			@Suspended final AsyncResponse asyncResponse) {
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
 				try {
-					String a = runSh("cd " + buildPath + " && docker build -t "
-							+ name + " .");
+					String a = runSh("cd " + buildPath + " && docker build -t " + name + " .");
 					asyncResponse.resume(a);
 				} catch (IOException e) {
 					e.printStackTrace();
@@ -137,8 +138,7 @@ public class Node {
 
 	@POST
 	@Path("save")
-	public void save(@QueryParam("image") final String image,
-			@QueryParam("file") final String file,
+	public void save(@QueryParam("image") final String image, @QueryParam("file") final String file,
 			@Suspended final AsyncResponse asyncResponse) {
 		new Thread(new Runnable() {
 
@@ -157,8 +157,7 @@ public class Node {
 
 	@POST
 	@Path("load")
-	public void load(@QueryParam("file") final String file,
-			@Suspended final AsyncResponse asyncResponse) {
+	public void load(@QueryParam("file") final String file, @Suspended final AsyncResponse asyncResponse) {
 		new Thread(new Runnable() {
 
 			@Override
@@ -175,56 +174,25 @@ public class Node {
 	}
 
 	public String runSh(String sh) throws IOException {
-		rwl.readLock().lock();
 		String ip = island.getIp();
 		boolean auth = false;
 		if (connection == null) {
-			rwl.readLock().unlock();
-			rwl.writeLock().lock();
-			if (connection == null) {
-				try {
-					connection = new Connection(
-							ip.substring(0, ip.indexOf(":")));
-					connection.connect();
-					// auth =
-					// connection.authenticateWithPassword(island.getUser(),
-					// island.getPass());
-					String ppk = Sea.getJedis().get("private_key");
-					auth = connection.authenticateWithPublicKey(
-							island.getUser(), ppk.toCharArray(), "");
-					
-					Timer timer = new Timer();
-					timer.schedule(new TimerTask() {
-						
-						@Override
-						public void run() {
-							rwl.writeLock().lock();
-							
-							System.out.println("close");
-							connection.close();
-							connection = null;
-							
-							
-							rwl.writeLock().unlock();
-						}
-					}, 1000*60*5);
-					
-					
-				} catch (IOException e) {
-					e.printStackTrace();
-					connection.close();
-					connection = null;
-					throw new IOException(
-							"Please config the /etc/ssh/sshd_config and restart sshd",
-							e);
-				}
-				if (!auth) {
-					connection = null;
-					throw new AuthenticationException("Wrong password");
-				}
+			try {
+				connection = new Connection(ip.substring(0, ip.indexOf(":")));
+				connection.connect();
+				// auth = connection.authenticateWithPassword(island.getUser(),
+				// island.getPass());
+				String ppk = Sea.getJedis().get("private_key");
+				auth = connection.authenticateWithPublicKey(island.getUser(), ppk.toCharArray(), "");
+			} catch (IOException e) {
+				e.printStackTrace();
+				connection = null;
+				throw new IOException("Please config the /etc/ssh/sshd_config and restart sshd", e);
 			}
-			rwl.writeLock().unlock();
-			rwl.readLock().lock();
+			if (!auth) {
+				connection = null;
+				throw new AuthenticationException("Wrong password");
+			}
 		}
 
 		Session session = null;
@@ -239,12 +207,10 @@ public class Node {
 		session.execCommand(sh);
 
 		InputStream stdoutInputStream = new StreamGobbler(session.getStdout());
-		BufferedReader stdoutBufferedReader = new BufferedReader(
-				new InputStreamReader(stdoutInputStream));
+		BufferedReader stdoutBufferedReader = new BufferedReader(new InputStreamReader(stdoutInputStream));
 
 		InputStream stderrInputStream = new StreamGobbler(session.getStderr());
-		BufferedReader stderrBufferedReader = new BufferedReader(
-				new InputStreamReader(stderrInputStream));
+		BufferedReader stderrBufferedReader = new BufferedReader(new InputStreamReader(stderrInputStream));
 
 		StringBuilder builder = new StringBuilder();
 		String firstLine = stdoutBufferedReader.readLine();
@@ -255,39 +221,31 @@ public class Node {
 		int blankTime = 5;
 		while (true) {
 			String line = stdoutBufferedReader.readLine();
-			logger.info(line);
-			if (line == null && blankTime == 0) {
-				logger.info("END");
+			if (line == null && blankTime == 0)
 				break;
-			}
 			if (line != null) {
 				builder.append(line);
 				builder.append(System.getProperty("line.separator"));
-			} else {
-				--blankTime;
 			}
+			--blankTime;
 		}
 
 		blankTime = 5;
 		while (true) {
 			String line = stderrBufferedReader.readLine();
-			logger.error(line);
-			if (line == null && blankTime == 0) {
-				logger.error("END");
+			if (line == null && blankTime == 0)
 				break;
-			}
 			if (line != null) {
 				builder.append(line);
 				builder.append(System.getProperty("line.separator"));
-			} else {
-				--blankTime;
 			}
+			--blankTime;
 		}
 
 		stdoutBufferedReader.close();
 		stderrBufferedReader.close();
 		session.close();
-		rwl.readLock().unlock();
+
 		return builder.toString();
 	}
 
